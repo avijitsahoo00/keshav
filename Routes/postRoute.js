338cca6ddd0002ea125f220cf6cc61aea3205551
avijitsahoo00 <avijitsahoo.hello.world@gmail.com>
2022-05-05 22:56:00 +0530

    add data1

--- Routes/postRoute.js
+++ Routes/postRoute.js
@@ -2,23 +2,75 @@ const express = require('express');
 const postRoute = express.Router();
 const blogSchema = require('../Model/blogSchema');
 
-postRoute.post('/new', (req, res) => {
-    console.log(req)
-//    const {title,categories,content} =  req.body ;
-//    const postData = {title,categories,content}
-//    try{
-//        const newPost = new blogSchema(postData)
-//        const response = await postData.save();
-//        res.send(response)
-//    }
-//    catch{
-//        console.log(error)
-//        res.send(error)
-//    }
+postRoute.post('/new', async (req, res) => {
+    console.log(req.body)
+   const {title,categories,content} =  req.body ;
+   const postData = {title,categories,content}
+   try{
+       const newPost = new blogSchema(postData)
+       const response = await newPost.save();
+       res.send(response)
+   }
+   catch(error){
+       console.log(error)
+       res.send(error)
+   }
 })
 
-postRoute.post("/edit/:id", (req, res) => {
+postRoute.post("/edit/:id", async (req, res) => {
+    // const {title,categories,content} =  req.body ;
+    console.log(req.body);
+    const postData = req.body ;
+    const id = req.params.id
+    try {
+        const response1 = await blogSchema.findById(id);
+
+       const response = await response1.update(postData);
+        res.send(response)
+    } catch (error) {
+        res.send(error)
+    }
+});
+
+postRoute.get("/get", async (req, res) => {
+    // const {title,categories,content} =  req.body ;
+    console.log(req.body);
+
+    try {
+
+       const response = await blogSchema.find();
+        res.send(response)
+        
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+postRoute.get("/get:Id", async (req, res) => {
+    // const {title,categories,content} =  req.body ;
+    console.log(req.body);
+
+    try {
+        const id = req.params.id
+       const response = await blogSchema.findById(id);
+        res.send(response)
+        
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+postRoute.post("/delete", async (req, res) => {
+
+    try {
+        const id = req.body.id;
+        const response = await blogSchema.deleteById(id);
+        res.send(response)
+    } catch (error) {
+        res.send(error)
+    }
 
 })
+
 
 module.exports = postRoute
